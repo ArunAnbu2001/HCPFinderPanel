680c67374cd80d6f44607d59ac5485329a89e6cc
Siva04-coder <80594440+Siva04-coder@users.noreply.github.com>
2022-09-27 11:16:59 +0530

    Update application.py

--- application.py
+++ application.py
@@ -33,63 +33,80 @@ def login():
 def register_click():
     return render_template('register1.html')
 
+@application.route('/logon', methods=['POST'])
+def logon():
+    res = False
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
-@app.route("/get_register_table" ,methods=["GET","POST"])
+        res, usertype, username = db_proxy.check_user(email, password)
+        print(usertype)
+        session['UserType'] = usertype
+        session['UserName'] = username
+
+        if res == True:
+            login_users.append(email)
+            session["email"] = email
+
+    return json.dumps(res)
+
+@application.route("/get_register_table" ,methods=["GET","POST"])
 def get_register_table():
     intent=db_proxy.get_register_table()
     return intent.to_json(orient="index")
 
-@app.route('/bulk_upload', methods=['GET', 'POST'])
+@application.route('/bulk_upload', methods=['GET', 'POST'])
 def bulk_upload():
     intent=db_proxy.bulk_upload()
     return json.dumps(intent)
 
-@app.route('/reject_doctor', methods=['GET', 'POST'])
+@application.route('/reject_doctor', methods=['GET', 'POST'])
 def reject_doctor():
     intent=db_proxy.reject_doctor()
     return json.dumps(intent)
 
-@app.route('/reject_reason_doctor', methods=['GET', 'POST'])
+@application.route('/reject_reason_doctor', methods=['GET', 'POST'])
 def reject_reason_doctor():
     intent=db_proxy.reject_reason_doctor()
     return json.dumps(intent)
 
-@app.route('/approve_doctor', methods=['GET', 'POST'])
+@application.route('/approve_doctor', methods=['GET', 'POST'])
 def approve_doctor():
     intent=db_proxy.approve_doctor()
     return json.dumps(intent)
 
-@app.route('/pending_doctor', methods=['GET', 'POST'])
+@application.route('/pending_doctor', methods=['GET', 'POST'])
 def pending_doctor():
     intent=db_proxy.pending_doctor()
     return json.dumps(intent)
 
-@app.route('/delete_doctor', methods=['GET', 'POST'])
+@application.route('/delete_doctor', methods=['GET', 'POST'])
 def delete_doctor():
     intent=db_proxy.delete_doctor()
     return json.dumps(intent)
 
-@app.route('/update_data', methods=['POST'])
+@application.route('/update_data', methods=['POST'])
 def update_data():
     res = db_proxy.update_data()
     return json.dumps(res)
 
-@app.route('/register', methods=['POST'])
+@application.route('/register', methods=['POST'])
 def register():  
     res = db_proxy.register()
     return json.dumps(res)
 
-@app.route('/change_password', methods=['POST'])
+@application.route('/change_password', methods=['POST'])
 def change_password():
     res = db_proxy.change_password()
     return json.dumps(res)
 
-@app.route('/update_profile', methods=['POST'])
+@application.route('/update_profile', methods=['POST'])
 def update_profile():
     res = db_proxy.update_profile()
     return json.dumps(res)
 
-@app.route('/fetch', methods=['GET'])
+@application.route('/fetch', methods=['GET'])
 def fetch():
     res = db_proxy.fetch()
     return json.dumps(res)
